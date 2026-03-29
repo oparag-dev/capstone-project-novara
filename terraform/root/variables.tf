dd7bb165bcda702c1eee49a89d93c779832de1ce
oparag-dev <oparagospel001@gmail.com>
2026-03-29 16:01:14 +0100

    Added files

--- terraform/root/variables.tf
+++ terraform/root/variables.tf
@@ -1,11 +1,35 @@
-variable "project_name" {
-  description = "Project name used for tagging and naming AWS resources"
-  type        = string
-  default     = "taskapp-tsa-terra"
-}
-
 variable "aws_region" {
   description = "AWS region to deploy into"
   type        = string
   default     = "eu-west-3"
+}
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "capstone-project-novara"
+}
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnet_cidr_blocks" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+variable "private_subnet_cidr_blocks" {
+    description = "List of CIDR blocks for private subnets"
+    type        = list(string)
+    default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"] 
+}
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
 }
